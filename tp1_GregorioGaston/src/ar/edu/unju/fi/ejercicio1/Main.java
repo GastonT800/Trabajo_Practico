a81ab64170c03e9143d313c5df65bf963ce8bbd5
GastonT800 <m.nick5164@gmail.com>
2024-04-15 10:49:32 -0300

    corrección en el ejercicio1

--- tp1_GregorioGaston/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_GregorioGaston/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -5,33 +5,22 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		//System.out.println("Hola Mundo");
-		
-		Scanner sc = new Scanner(System.in);
-		
+	
 		//Definimos variables a usar
-		int num1, num2, num3, num4, num5;
+		int num1= 3;
+		int num2= 13;
+		int num3= 8;
+		int num4= 5;
+		int num5= 24;
 		double promedio;
 		
-		// Pedimos al usuario que ingrese los valores eh asignamos a las variables ya inicializadas
-		System.out.print("Ingrese valor del primer número: ");
-		num1 = sc.nextInt();
-		System.out.print("Ingrese valor del segundo número: ");
-		num2 = sc.nextInt();
-		System.out.print("Ingrese valor del tercer número: ");
-		num3 = sc.nextInt();
-		System.out.print("Ingrese valor del cuarto número: ");
-		num4 = sc.nextInt();
-		System.out.print("Ingrese valor del quinto número: ");
-		num5 = sc.nextInt();
 		
-		//Sacamos el promedio de los valores
+		
+		//Calculamos el promedio de los valores
 		promedio = (num1 + num2 + num3 + num4 + num5)/5.0;
 		
 		//Mostramos por pantalla el valor de promedio
 		System.out.print("El promedio es: " + promedio);
-		
-		sc.close();
 	}
 
 }
