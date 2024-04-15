5ec4eea48f888dd26714666d8e759e2765dc64af
GastonT800 <m.nick5164@gmail.com>
2024-04-15 11:21:08 -0300

    se creó el package ejercicio3

--- tp1_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/Main.java
+++ tp1_GregorioGaston/src/ar/edu/unju/fi/ejercicio3/Main.java
@@ -1,10 +1,35 @@
 package ar.edu.unju.fi.ejercicio3;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+	
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
 		
+		//Definimos variables
+		int num, numPar, numImpar;
+		
+		//Solicitamos al usuario por medio de consola que ingrese un Número
+		System.out.print("Ingrese un Número: ");
+		num = sc.nextInt();
+		
+		if(num % 2 == 0) {
+			
+			numPar = num*3;
+			
+			System.out.println("El número ingresado es par, el triple es: " + numPar);
+			
+		}else {
+			
+			numImpar = num*2;
+			
+			System.out.println("El número ingresado es impar, el doble es: " + numImpar);
+		}
+		
+		
+		sc.close();
 	}
 
 }
